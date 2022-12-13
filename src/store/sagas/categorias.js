f52aa61ae1d32110559e58a4259fcd329f0fb8c6
Luiz Fernando <lfrprazeres@gmail.com>
2022-12-12 21:11:16 -0300

    3.1 - Aproveitando os toasts

--- src/store/sagas/categorias.js
+++ src/store/sagas/categorias.js
@@ -1,8 +1,32 @@
 import { takeEvery } from 'redux-saga/effects';
 import { buscarCategorias } from 'store/reducers/categorias';
+import { createStandaloneToast } from '@chakra-ui/toast';
+
+const { toast } = createStandaloneToast();
 
 function* observarCategorias() {
   yield console.log('observando');
+  toast({
+    title: 'Carregando',
+    description: 'Carregando categorias',
+    status: 'loading',
+    duration: 2000,
+    isClosable: true
+  });
+  toast({
+    title: 'Sucesso!',
+    description: 'Categorias carregadas com sucesso!',
+    status: 'success',
+    duration: 2000,
+    isClosable: true
+  });
+  toast({
+    title: 'Erro',
+    description: 'Erro na busca de categorias',
+    status: 'error',
+    duration: 2000,
+    isClosable: true
+  });
 }
 
 export function* categoriasSaga() {
