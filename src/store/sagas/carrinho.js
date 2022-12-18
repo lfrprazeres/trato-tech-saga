0ad636fb7ab4e9db05c6f9fea68d4d390802ca5b
Luiz Fernando <lfrprazeres@gmail.com>
2022-12-17 23:16:34 -0300

    5.1 - Pegando os dados

--- src/store/sagas/carrinho.js
+++ src/store/sagas/carrinho.js
@@ -1,8 +1,23 @@
-import { takeLatest } from 'redux-saga/effects';
+import { call, takeLatest } from 'redux-saga/effects';
+import bandeirasService from 'services/bandeiras';
+import cartoesService from 'services/cartoes';
+import usuariosService from 'services/usuarios';
 import { carregarPagamento } from 'store/reducers/carrinho';
 
+const usuarioLogado = 1;
+
 function* carregarPagamentoSaga() {
-  yield console.log('carregando pagamento');
+  try {
+    const usuario = yield call(usuariosService.buscarPorId, usuarioLogado);
+    const cartoes = yield call(cartoesService.buscarPorIdUsuario, usuarioLogado);
+    const bandeiraIds = cartoes.map(cartao => cartao.bandeiraId);
+    const bandeiras = yield call(bandeirasService.buscarPorId, bandeiraIds);
+    const cartoesComBandeiras = cartoes.map(cartao => {
+      const bandeiraDoCartao = bandeiras.find(bandeira => bandeira.id === cartao.bandeiraId);
+      return { ...cartao, taxa: bandeiraDoCartao.taxa, bandeira: bandeiraDoCartao.nome };
+    });
+    console.log({ ...usuario, cartoes: cartoesComBandeiras });
+  } catch(e) {}
 }
 
 export function* carrinhoSaga() {
